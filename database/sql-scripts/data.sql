39f829a067497441aeb4adc0ebc5153ddf6efd94
roger2113 <mva2113@gmail.com>
2017-11-10 20:57:04 +0300

    dealing with docker

--- database/sql-scripts/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (firstname, lastname, login, email, password, role) VALUES ('Ronald', 'Tremp', 'ron', 'onlyaftervova@starpost.gov', 'qwerty', 'USER');
-INSERT INTO images (name, user_id) VALUES ('The Second', 1);
-INSERT INTO galleries (name, user_id) VALUES ('The First Gallery', 1);
-INSERT INTO image_gallery (gallery_id, image_id ) VALUES (1, 1);
